7fd51daeb49c53d400760c784329ff49f0ccb5cf
VishalSKumar <vishalskumar12@gmail.com>
2017-05-23 16:57:14 +0700

    Added test for DoubleWrapper

--- src/main/scala/fiddle/DoubleWrapper.scala
+++ src/main/scala/fiddle/DoubleWrapper.scala
@@ -5,15 +5,3 @@ case class Precision(precision: Double)
 class DoubleWrapper(val x: Double) {
   def ~=(that: Double)(implicit p: Precision): Boolean = (this.x - that).abs <= p.precision
 }
-
-object DoubleWrapperMain {
-
-  implicit def doubleToDoubleWrapper(d: Double): DoubleWrapper = new DoubleWrapper(d)
-
-  implicit val precision = Precision(0.001)
-
-  def main(args: Array[String]): Unit = {
-    if (0.0 ~= 0.00001)
-      println("They Are equal")
-  }
-}
---
+++ src/test/scala/fiddle/DoubleWrapperTest.scala
@@ -0,0 +1,13 @@
+package fiddle
+
+import org.scalatest.FunSuite
+
+class DoubleWrapperTest extends FunSuite {
+
+  test("double value comparison"){
+    implicit def doubleToDoubleWrapper(d: Double): DoubleWrapper = new DoubleWrapper(d)
+    implicit val precision = Precision(0.001)
+    assert(0.0 ~= 0.00001)
+  }
+
+}
