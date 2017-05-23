339dc309ea7c085978b8da132d1ad22f332fc703
VishalSKumar <vishalskumar12@gmail.com>
2017-05-23 14:39:30 +0700

    Added Median calculation fiddle

--- src/main/scala/NumbersFiddle.scala
+++ src/main/scala/NumbersFiddle.scala
@@ -6,3 +6,14 @@ class NumbersFiddle(numbers: Vector[Int]) {
   lazy val y = numbers.sum
   lazy val z = Math.sqrt(y)
 }
+
+object NumbersFiddle {
+  def apply(vector: Vector[Int]) = new NumbersFiddle(vector)
+
+  def median(seq: Seq[Int]): Double =
+    if (seq.size % 2 == 0) {
+      val mid = seq.size / 2
+      (seq(mid) + seq(mid - 1)) / 2.0
+    } else
+      seq(seq.size / 2)
+}
--- src/test/scala/NumbersFiddleTest.scala
+++ src/test/scala/NumbersFiddleTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 class NumbersFiddleTest extends FlatSpec {
 
   val testVector = Vector(1, 32,45, 7, 3)
-  val numbersFiddle = new NumbersFiddle(testVector)
+  val numbersFiddle = NumbersFiddle(testVector)
   it should "create a vector with square of all elements" in {
     assert(numbersFiddle.x == Vector(1, 1024, 2025, 49, 9))
   }
@@ -13,5 +13,10 @@ class NumbersFiddleTest extends FlatSpec {
   }
   it should "square root the sum of all elements of vector" in {
     assert(numbersFiddle.z == 9.38083151964686)
+  }
+
+  it should "return median of sequence" in {
+    assert(NumbersFiddle.median(1 to 5) == 3)
+    assert(NumbersFiddle.median(1 to 4) == 2.5)
   }
 }
